d89194334a2b1d68565d126e371b77f88c0de876
Arnold Amasoh <amasoharnold@gmail.com>
2020-08-01 12:21:34 -1100

    added sign up component and integrated firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom'
-import { auth } from './firebase/firebase.util'
+import { auth, createUserProfileDocument } from './firebase/firebase.util'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
@@ -20,9 +20,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: snapShot.id,
+            ...snapShot.data()
+          })
+          console.log(this.state)
+        })
+
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -13,6 +13,32 @@ const config = {
     measurementId: "G-7JYFD3FYSV"
 };
 
+export const createUserProfileDocument = async (userAuth, AdditionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...AdditionalData
+            })
+        } catch (err) {
+            console.log('error creating user', err.message)
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
